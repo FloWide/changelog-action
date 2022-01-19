9b1919b4a962f020a1218efec091ce17d210f5d3
Balázs Görzsöny <bgorzsony@gmail.com>
2022-01-19 10:29:41 +0100

    generator script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:3.10
 
+RUN apk --no-cache add git 
+
 COPY LICENSE README.md /
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,24 @@
 #!/bin/sh -l
 
-echo "hello $1"
+if [ -z "$FROM_TAG" ]; then
+  from_tag=$(git tag --sort version:refname | tail -n 2 | head -n 1)
+else
+  from_tag=$FROM_TAG
+fi
+
+if [ -z "$TO_TAG" ]; then
+  to_tag=$(git tag --sort version:refname | tail -n 1)
+else
+  to_tag=$TO_TAG
+fi
+
+
+echo "Generating changelog from commits between $from_tag and $to_tag"
+
+changes="#### Changes (${to_tag}):\n"
+changes="${changes}$(git log $from_tag...$to_tag --pretty=format:'- %s (%H)\n' --reverse | grep -v Merge )"
+
+echo $changes
+
+echo "::set-output name=changelog::$changes"
+
